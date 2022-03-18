d9a6bd2d0158b0c4529301a44d421aa44b431fcc
Jordan bhar <bharjor@sheridancollege.ca>
2022-03-18 16:06:12 -0400

    Commit 0: created some of the required files for now, mainly focusing on HTML rightnow

---
+++ src/main/java/sheridan/bharjor/ca/cat_dogroleapplication/CatDogRoleApplication.java
@@ -0,0 +1,13 @@
+package sheridan.bharjor.ca.cat_dogroleapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatDogRoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatDogRoleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sheridan/bharjor/ca/cat_dogroleapplication/CatDogRoleApplicationTests.java
@@ -0,0 +1,13 @@
+package sheridan.bharjor.ca.cat_dogroleapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatDogRoleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
